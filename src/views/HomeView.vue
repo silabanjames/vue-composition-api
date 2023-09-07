c73c3c990daf6f778dfc44f367dfe89746790604
silabanjames <silaban.james13@gmail.com>
2023-09-07 16:07:35 +0700

    methods, computed, and watch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,14 @@
     <h3>{{ counterData.counterTitle }}:</h3>
 
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
       <span class="counter">{{ counterData.counter }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
     </div>
+
+    <p>The counter is {{ oddOrEven }}</p>
 
     <div class="edit">
       <h3>Edit counter title:</h3>
@@ -22,28 +26,62 @@
     </div>
   </div>
 
-    
 </template>
 
 <!-- Composition API | Script Setup Pattern -->
 <script setup>
-import { ref, reactive } from 'vue'
+import { reactive, computed, watch } from 'vue'
 
-const appTitle = 'My amazing App'
+const appTitle = 'My amazing App';
 
 const counterData = reactive({
   counter: 0,
   counterTitle : "My Counter"
 })
 
-const increaseCounter = () => {
-  counterData.counter++;
+
+// show alert if counter reach 20
+watch(() => counterData.counter, (newCount, oldCount) => {
+  if(newCount == 20) alert('Way to go! You made it to 20!!');
+})
+
+
+// computed variable that show the counter odd or even
+const oddOrEven = computed(() => {
+  if (counterData.counter%2 == 0) return 'even';
+  else return 'odd';
+});
+
+const increaseCounter = (amount, e) => {
+  counterData.counter += amount;
 }
 
-const decreaseCounter = () => {
-  counterData.counter--;
+const decreaseCounter = amount => {
+  counterData.counter -= amount;
 }
 </script>
+
+<!-- COMPUTED PROPERTIES IN OPTIONS API -->
+<!-- <script>
+export default {
+  data(){
+    return {
+      count: 0,
+    }
+  },
+  computed: {
+    myComputedProperty(){
+      // perform logic based on a data property
+      return 'my result';
+    }
+  },
+  watch:{
+    count(newCount, oldCount){
+      if (newCount == 20) alert('asdasd')
+    }
+  }
+}
+</script>  -->
 
 <style>
 .home{
