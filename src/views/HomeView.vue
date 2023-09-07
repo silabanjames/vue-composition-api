740f14fff945364ee56f475465230ebc70e710d8
silabanjames <silaban.james13@gmail.com>
2023-09-07 09:53:56 +0700

    options API vs compositions API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,84 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
   </div>
 </template>
+
+<!-- Composition API | Script Setup Pattern -->
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0);
+
+const increaseCounter = () => {
+  counter.value++;
+}
+
+const decreaseCounter = () => {
+  counter.value--;
+}
+</script>
+
+<!-- Composition API | Setup Function Pattern -->
+<!-- <script>
+import { ref } from 'vue'
+
+export default {
+  setup(){
+
+    const counter = ref(0);
+
+    const increaseCounter = () => {
+      console.log(counter);
+      counter.value++;
+    }
+
+    const decreaseCounter = () => {
+      counter.value--;
+    }
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter
+    }
+  }
+}
+</script> -->
+
+<!-- Options API -->
+<!--
+<script>
+export default {
+  data(){
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    increaseCounter(){
+      this.counter++;
+    },
+    decreaseCounter(){
+      this.counter--;
+    }
+  }
+}
+</script>
+-->
+
+<style>
+.home{
+  text-align: center;
+  padding: 10px;
+}
+
+.btn, .counter{
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
